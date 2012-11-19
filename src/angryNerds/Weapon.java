b66c450f9c5edb69fc6d03ce325f96e3467b7646
Orion Kostival <okostiva@mines.edu>
2012-11-19 14:34:46 -0700

    Clean up

--- src/angryNerds/Weapon.java
+++ src/angryNerds/Weapon.java
@@ -8,21 +8,28 @@ public abstract class Weapon {
 
 	protected int damage;
 	protected Image image;
+	protected int x, y;
 	
 	public Weapon() {
 		// TODO Auto-generated constructor stub
 		damage = 0;
+		x = 0;
+		y = 0;
 		image = Toolkit.getDefaultToolkit().getImage("/images/none.jpg");
 	}
 	
 	public Weapon(int damage, Image image) {
 		this.damage = damage;
 		this.image = image;
+		x = 0;
+		y = 0;
 	}
 	
 	public Weapon(int damage, String imagePath) {
 		this.damage = damage;
 		this.image = Toolkit.getDefaultToolkit().getImage(imagePath);
+		x = 0;
+		y = 0;
 	}
 	
 	public int getDamage() {
